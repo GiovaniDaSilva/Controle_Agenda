60bbd8cf38e4fdaecfadc3fb05cbfc4ed83105e4
Giovani <giovani-vani@hotmail.com>
2020-01-19 10:15:17 -0300

    Ajustes no layout da tela de html

--- Agenda/Formularios/frmHTML.Designer.vb
+++ Agenda/Formularios/frmHTML.Designer.vb
@@ -22,41 +22,69 @@ Partial Class frmHTML
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmHTML))
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         Me.btnOK = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'RichTextBox1
         '
-        Me.RichTextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.RichTextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 55)
         Me.RichTextBox1.Name = "RichTextBox1"
         Me.RichTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical
-        Me.RichTextBox1.Size = New System.Drawing.Size(637, 312)
+        Me.RichTextBox1.Size = New System.Drawing.Size(637, 361)
         Me.RichTextBox1.TabIndex = 0
         Me.RichTextBox1.Text = ""
         '
         'btnOK
         '
-        Me.btnOK.Location = New System.Drawing.Point(292, 330)
+        Me.btnOK.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnOK.BackColor = System.Drawing.SystemColors.Control
+        Me.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnOK.Image = CType(resources.GetObject("btnOK.Image"), System.Drawing.Image)
+        Me.btnOK.Location = New System.Drawing.Point(285, 424)
         Me.btnOK.Name = "btnOK"
-        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.Size = New System.Drawing.Size(75, 58)
         Me.btnOK.TabIndex = 1
-        Me.btnOK.Text = "OK"
-        Me.btnOK.UseVisualStyleBackColor = True
+        Me.btnOK.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(637, 37)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "- Com a consulta de Solicitações carregada, clique com o botão direito e vá para " &
+    "Exibir Código Fonte. " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "- Copie todo o conteudo e cole abaixo."
         '
         'frmHTML
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(661, 381)
+        Me.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.ClientSize = New System.Drawing.Size(661, 494)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.btnOK)
         Me.Controls.Add(Me.RichTextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "frmHTML"
-        Me.Text = "frmHTML"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Informe o HTML da Consulta de Solicitações"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents RichTextBox1 As RichTextBox
     Friend WithEvents btnOK As Button
+    Friend WithEvents TextBox1 As TextBox
 End Class
--- Agenda/Formularios/frmHTML.vb
+++ Agenda/Formularios/frmHTML.vb
@@ -2,14 +2,9 @@
     Dim HTML As String
 
     Public Function RetornarHTML() As String
-
-
         Me.ShowDialog()
-
-
+        HTML = RichTextBox1.Text
         Return HTML
-
-
     End Function
 
     Private Sub RichTextBox1_Leave(sender As Object, e As EventArgs) Handles RichTextBox1.Leave
@@ -17,6 +12,7 @@
     End Sub
 
     Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        HTML = RichTextBox1.Text
         Me.Close()
     End Sub
 End Class
