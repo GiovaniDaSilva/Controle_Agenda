e197ae44e1024d6b5f91b96564b6d4ca4ca587ab
Giovani <giovani-vani@hotmail.com>
2020-02-16 10:58:47 -0300

    Adicionado somatorio das horas inicial e final e dicionado a grid

--- Agenda/Classes/Serviços/Web/clsCadastroAtividadeWeb.vb
+++ Agenda/Classes/Serviços/Web/clsCadastroAtividadeWeb.vb
@@ -1,11 +1,9 @@
 ﻿Public Class clsCadastroAtividadeWeb
 
     Public Function RetornaPaginaCadastroAtividade() As String
-
         Return My.Resources.CadastroAtividade
-
-
     End Function
+
 
     Friend Function RetornaCadastroAtividade_Salvar(json As String) As String
 
@@ -15,23 +13,16 @@
         Return json.ToString
     End Function
 
-
-
-
-    'Private Function DeserializarNewtonsoft(json As String) As List(Of clsPeriodoWeb)
-    ' 'Dim Json = System.IO.File.ReadAllText("pedidos2.json")
-    ' Return Newtonsoft.Json.JsonConvert.DeserializeObject(Of List(Of clsPeriodoWeb))(json)
-    ' End Function
-
     Private Function DeserializarNewtonsoft(json As String) As clsAtividadeWeb
-        'Dim Json = System.IO.File.ReadAllText("pedidos2.json")
         Return Newtonsoft.Json.JsonConvert.DeserializeObject(Of clsAtividadeWeb)(json)
     End Function
 
 End Class
 
 
-
+''' <summary>
+''' Classes para carregar a partir do json
+''' </summary>
 Public Class clsAtividadeWeb
     Public Property dataAtividade As Date
     Public Property tipoAtividade As Integer
@@ -51,6 +42,3 @@ Public Class clsPeriodoWeb
     Public Property Ate As String
     Public Property Total As String
 End Class
-
-'{"dataAtividade""","tipoAtividade":"1","codigoAtividade":"","horaTotal":"","horaInicio":"","horaFinal":"","descricaoAtividade":"","
-'Periodo":[{"ID":"1","De":"08:00","Até":"10:30","Total":"02:30"},{"ID":"2","De":"08:00","Até":"10:30","Total":"02:30"},{"ID":"3","De":"08:00","Até":"10:30","Total":"02:30"},{"ID":"4","De":"08:00","Até":"10:30","Total":"02:30"},{"ID":"5","De":"08:00","Até":"10:30","Total":"02:30"},{"ID":"6","De":"08:00","Até":"10:30","Total":"02:30"},{"ID":"7","De":"08:00","Até":"10:30","Total":"02:30"},{"ID":"8","De":"08:00","Até":"10:30","Total":"02:30"}]}
