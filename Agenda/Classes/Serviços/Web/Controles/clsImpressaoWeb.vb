422296349762e5d60d68c17df7df24734a423d1a
Giovani da Silva <giovani.silva@govbr.com.br>
2020-03-13 17:09:13 -0300

    adicao da classe de impressao

---
+++ Agenda/Classes/Serviços/Web/Controles/clsImpressaoWeb.vb
@@ -0,0 +1,34 @@
+﻿Public Class clsImpressaoWeb
+
+    Public Function RetornaPagina(pAtividade As clsAtividade) As String
+            Return RetornaHTML(pAtividade)
+        End Function
+
+        Private Function RetornaHTML(pAtividade As clsAtividade) As String
+
+            Dim html As String
+            html = My.Resources.Impressao
+            'html = html.Replace("[p_inicializa_campos_atividade]", RetornaLinhasInicializacaoCampos(pAtividade))
+
+
+            Dim x As String
+
+            Dim ini = New clsIni().funCarregaIni
+            Dim atividades = New clsAdicionarDAO().carregarAtividades(pAtividade, ini)
+
+
+            For Each item In atividades
+                x &= "&nbsp <b>codigo:</b> " & item.Codigo
+                x &= "</br> &nbsp <b>tipo:</b> " & item.TIPO_DESCRICAO
+                x &= "</br> &nbsp <b>hora: </b>" & item.Horas
+                x &= "</br> &nbsp <b>descricao: </b>" & item.Descricao
+                x &= "</br> </br>"
+            Next
+
+
+            html = html.Replace("{p_dados_impresso}", x)
+            Return html
+        End Function
+
+
+    End Class
--- Agenda/Classes/Serviços/Web/clsRequisicoesWeb.vb
+++ Agenda/Classes/Serviços/Web/clsRequisicoesWeb.vb
@@ -51,28 +51,10 @@ Public Class clsRequisicoesWeb
 
     Private Function funRetornaPaginaImpressao(pReqWeb As clsReqWeb) As String
 
-        Dim retorno As String
-        retorno = My.Resources.Impressao
-        Dim x As String
-
         Dim filtro As New clsAtividade
-        Dim ini = New clsIni().funCarregaIni
         filtro.Data = Now.AddDays(-7)
 
-        Dim atividades = New clsAdicionarDAO().carregarAtividades(filtro, ini)
-
-        For Each item In atividades
-            x &= "&nbsp <b>codigo:</b> " & item.Codigo
-            x &= "</br> &nbsp <b>tipo:</b> " & item.TIPO_DESCRICAO
-            x &= "</br> &nbsp <b>hora: </b>" & item.Horas
-            x &= "</br> &nbsp <b>descricao: </b>" & item.Descricao
-            x &= "</br> </br>"
-        Next
-
-
-
-        retorno = retorno.Replace("{p_dados_impresso}", x)
-        Return retorno
+        Return New clsImpressaoWeb().RetornaPagina(filtro)
     End Function
 
     Private Function funRetornaDetalhesAtividade(pReqWeb As clsReqWeb) As String
